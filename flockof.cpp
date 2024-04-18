74549c7aa6f4ab1e34554acd979dcbadb0c4421c
stirodaferro <167412093+stirodaferro@users.noreply.github.com>
2024-04-18 16:09:59 +0200

    Update flockof.cpp

--- flockof.cpp
+++ flockof.cpp
@@ -1,52 +1,34 @@
-#include <math.h>
 #include "flockof.hpp"
-#include <vector>
+#include <math.h>
 #include <algorithm>
-#include <numeric>
 #include <iostream>
+#include <numeric>
 
-
-
-struct position{
-    double x;
-    double y;
-};
-
-struct velocity{
-    double v_x;
-    double v_y;
-};
-
-class boid{
-public:
-   velocity vb;
-   position xb;
-};
-
-double distance(position const& a, position const& b){
-    return std::sqrt(std::pow(a.x-b.x,2)+std::pow(a.y-b.y,2));
+double distance(position const& a, position const& b)
+{
+  return std::sqrt(std::pow(a.x - b.x, 2) + std::pow(a.y - b.y, 2));
 }
 
-position vector_distance(position const& a, position const& b){
-    return {a.x-b.x, a.y-b.y};
+position vector_distance(position const& a, position const& b)
+{
+  return {a.x - b.x, a.y - b.y};
 }
 
-velocity sum_of_velocity(velocity a, velocity b){
-    return {a.v_x+b.v_x, a.v_y+b.v_y};
+velocity sum_of_velocity(velocity const& a, velocity const& b)
+{
+  return {a.v_x + b.v_x, a.v_y + b.v_y};
 }
 
-position cm(std::vector<position> const& boids, int N){
-    auto cm_x = std::accumulate(boids.begin(),boids.end(),0.,[](double res, position a){return res + a.x;});
-    auto cm_y = std::accumulate(boids.begin(), boids.end(), 0., [](double res, position a){return res + a.y;});
-    return {(cm_x)/(N-1),(cm_y)/(N-1)};
+position cm(std::vector<boid> const& boids, int N)
+{
+  auto cm_x = std::accumulate(boids.begin(), boids.end(), 0.,
+                              [](double res, boid a) { return res + a.pb.x; });
+  auto cm_y = std::accumulate(boids.begin(), boids.end(), 0.,
+                              [](double res, boid a) { return res + a.pb.y; });
+  return {(cm_x) / (N - 1), (cm_y) / (N - 1)};
 }
 
-velocity cohesion(double c, position CM, boid b){
-    return {c*(vector_distance(CM, b.xb).x), c*(vector_distance(CM, b.xb).y)};
-}
-
-int main(){
-    int N;
-    std::cout << "How many boids do you want?/n";
-    std::cin >> N;
+velocity cohesion(double c, position const& CM, boid const& b)
+{
+  return {c * (vector_distance(CM, b.pb).x), c * (vector_distance(CM, b.pb).y)};
 }
