86de057c3bd1ad3778e4f0bc2dfed3ad820f4e72
stirodaferro <167412093+stirodaferro@users.noreply.github.com>
2024-04-18 16:10:55 +0200

    Update flockof.hpp

--- flockof.hpp
+++ flockof.hpp
@@ -1,1 +1,34 @@
+#ifndef FLOCKOF_HPP
+#define FLOCKOF_HPP
+#include <vector>
 
+struct position
+{
+  double x;
+  double y;
+};
+
+struct velocity
+{
+  double v_x;
+  double v_y;
+};
+
+class boid
+{
+ public:
+  position pb;
+  velocity vb;
+};
+
+double distance(position const&, position const&);
+
+position vector_distance(position const&, position const&);
+
+velocity sum_of_velocity(velocity const&, velocity const&);
+
+position cm(std::vector<boid> const&, int);
+
+velocity cohesion(double, position const&, position const&);
+
+#endif
