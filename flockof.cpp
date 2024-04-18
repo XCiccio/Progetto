1073ed26409f5594671458adb6c7dbd987b6b6fa
stirodaferro <167412093+stirodaferro@users.noreply.github.com>
2024-04-18 12:03:16 +0200

    Update flockof.cpp

--- flockof.cpp
+++ flockof.cpp
@@ -1,3 +1,21 @@
-int main(){
-  return o;
+#include <math.h>
+#include "flockof.hpp"
+
+
+struct position{
+    double x;
+    double y;
+};
+
+struct velocity{
+    double v_x;
+    double v_y;
+};
+
+double sum_of_position(position const& a, position const& b){
+    return std::sqrt(std::pow(a.x-b.x,2)+std::pow(a.y-b.y,2));
+}
+
+velocity sum_of_velocity(velocity a, velocity b){
+    return {a.v_x+b.v_x, a.v_y+b.v_y};
 }
