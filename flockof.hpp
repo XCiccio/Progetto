a4803940a3f2c2be9bbed571762525ed79543f15
stirodaferro <167412093+stirodaferro@users.noreply.github.com>
2024-04-22 20:38:36 +0200

    Update flockof.hpp

--- flockof.hpp
+++ flockof.hpp
@@ -1,6 +1,13 @@
 #ifndef FLOCKOF_HPP
 #define FLOCKOF_HPP
+#include <math.h>
 #include <vector>
+
+struct statistics
+{
+  double mean_distance;
+  double mean_velocity;
+};
 
 struct position
 {
@@ -12,6 +19,16 @@ struct velocity
 {
   double v_x;
   double v_y;
+  double module()
+  {
+    return std::sqrt(std::pow(v_x, 2) + std::pow(v_y, 2));
+  }
+  velocity& operator+=(const velocity& v)
+  {
+    v_x + v.v_x;
+    v_y + v.v_y;
+    return *this;
+  }
 };
 
 class boid
@@ -40,5 +57,9 @@ position random_position_generator();
 velocity random_velocity_generator();
 
 std::vector<boid> boids_generator(int);
+
+double d_m(std::vector<boid>, int);
+
+double v_m(std::vector<boid>, int);
 
 #endif
