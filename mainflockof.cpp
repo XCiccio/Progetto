04ea488d7c430fca3692d32af0c1d5d80a7322ae
stirodaferro <167412093+stirodaferro@users.noreply.github.com>
2024-06-28 11:29:29 +0200

    Update mainflockof.cpp

--- mainflockof.cpp
+++ mainflockof.cpp
@@ -1,4 +1,5 @@
 #include "sfml_flockof.hpp"
+#include <cassert>
 #include <iostream>
 
 int main()
@@ -7,18 +8,38 @@ int main()
   std::cout << "How many boids do you want to generate? \n";
   int N;
   std::cin >> N;
+  if (std::cin.fail() || std::cin.peek() != '\n') {
+    return 1;
+  }
+  assert(N > 1);
   std::cout << "Insert the parameter s for separation \n";
   double s;
   std::cin >> s;
+  if (!std::cin.good()) {
+    return 1;
+  }
+  assert(s >= 0);
   std::cout << "Insert the parameter ds for minimum distance between boids \n";
   double ds;
   std::cin >> ds;
+  if (!std::cin.good()) {
+    return 1;
+  }
+  assert(ds >= 0);
   std::cout << "Insert the parameter a for alignment \n";
   double a;
   std::cin >> a;
+  if (!std::cin.good()) {
+    return 1;
+  }
+  assert(a >= 0);
   std::cout << "Insert the parameter c for cohesion \n";
   double c;
   std::cin >> c;
+  if (!std::cin.good()) {
+    return 1;
+  }
+  assert(c >= 0);
   sf::RenderWindow window(sf::VideoMode(800, 800), "FLOCK OF BOIDS");
   std::vector v = boids_generator(N);
   auto shapes   = create_shapes(v);
