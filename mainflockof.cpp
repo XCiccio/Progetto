8b2fba9021bbc10d671d3e07ed5349a1dff6f284
stirodaferro <167412093+stirodaferro@users.noreply.github.com>
2024-07-01 22:20:01 +0200

    Update mainflockof.cpp

--- mainflockof.cpp
+++ mainflockof.cpp
@@ -40,8 +40,13 @@ int main()
     return 1;
   }
   assert(c >= 0);
+  const double min_position{10.};
+  const double max_position{790.};
+  const double max_velocity{150.};
+  const double velocity_generation{100.};
+  const double frame_rate_limit{1./60.};
   sf::RenderWindow window(sf::VideoMode(800, 800), "FLOCK OF BOIDS");
-  std::vector v = boids_generator(N);
+  std::vector v = boids_generator(N, min_position, max_position, velocity_generation);
   auto shapes   = create_shapes(v);
   constexpr int frame_rate{60};
   window.setFramerateLimit(frame_rate);
@@ -52,7 +57,13 @@ int main()
         window.close();
       }
     }
-    v = update_boids(v, s, ds, a, c, N);
+    v = update_boids(v, s, ds, a, c, N, min_position, max_position, max_velocity, frame_rate_limit);
     update_window(window, shapes, v);
+    /*data positon_analysis  = position_data_analysis(v, N);
+    data velocity_analysis = velocity_data_analysis(v, N);
+    std::cout << "\rDistanza media dal centro di massa e velocità media: "
+              << positon_analysis.mean << " +/- " << positon_analysis.sigma
+              << "    " << velocity_analysis.mean << " +/- "
+              << velocity_analysis.sigma;*/
   }
 }
