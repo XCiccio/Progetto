2703ee35f528f31739f2d30759d3a4a9b1979937
XCiccio <67512558+XCiccio@users.noreply.github.com>
2024-07-03 09:16:14 +0200

    Add files via upload

---
+++ nuovoflockof.hpp
@@ -0,0 +1,116 @@
+#ifndef FLOCKOF_HPP
+#define FLOCKOF_HPP
+#include <math.h>
+#include <chrono>
+#include <memory>
+#include <vector>
+
+struct statistics
+{
+  double mean_distance;
+  double mean_velocity;
+};
+
+struct data
+{
+  double mean;
+  double sigma;
+};
+
+struct position
+{
+  double x;
+  double y;
+  position& operator+=(const position& p)
+  {
+    x = x + p.x;
+    y = y + p.y;
+    return *this;
+  }
+  position& operator-=(const position& p)
+  {
+    x = x - p.x;
+    y = y - p.y;
+    return *this;
+  }
+};
+
+position operator+(position const&, position const&);
+
+position operator-(position const&, position const&);
+
+position operator/(position const&, int);
+
+position operator*(position const&, double);
+
+struct velocity
+{
+  double v_x;
+  double v_y;
+  double module()
+  {
+    return std::sqrt(std::pow(v_x, 2) + std::pow(v_y, 2));
+  }
+  velocity& operator+=(const velocity& v)
+  {
+    v_x = v_x + v.v_x;
+    v_y = v_y + v.v_y;
+    return *this;
+  }
+  velocity& operator-=(const velocity& v)
+  {
+    v_x = v_x - v.v_x;
+    v_y = v_y - v.v_y;
+    return *this;
+  }
+};
+
+velocity operator+(velocity const&, velocity const&);
+
+velocity operator-(velocity const&, velocity const&);
+
+velocity operator*(double const, velocity const&);
+
+class boid
+{
+ public:
+  position pb;
+  velocity vb;
+};
+
+double distance(position const&, position const&);
+
+velocity sum_of_velocity(velocity const&, velocity const&);
+
+position cm(std::vector<boid> const&, int);
+
+velocity cohesion(int const N, double const, position const&, boid const&);
+
+velocity cohesion(std::vector<boid> const&, int const, double const,
+                  boid const&, double d);
+
+velocity alignment(double const, boid const&, std::vector<boid> const&,
+                   int const, double);
+
+velocity separation(double const, double const, boid const&,
+                    std::vector<boid> const&);
+
+position random_position_generator(const double, const double);
+
+velocity random_velocity_generator(const double);
+
+std::vector<boid> boids_generator(int, const double, const double,
+                                  const double);
+
+velocity wall_repulsion(boid, const double, const double);
+
+std::vector<boid> update_boids(std::vector<boid>&, double const, double const,
+                               double const, double const, double const,
+                               int const, const double, const double,
+                               const double, const double);
+
+data position_data_analysis(std::vector<boid> const&, int const);
+
+data velocity_data_analysis(std::vector<boid> const&, int const);
+
+#endif
---
+++ nuovommainflockof.cpp
@@ -0,0 +1,78 @@
+#include "sfml_flockof.hpp"
+#include <cassert>
+#include <iostream>
+
+int main()
+
+{
+  std::cout << "How many boids do you want to generate? \n";
+  int N;
+  std::cin >> N;
+  if (std::cin.fail() || std::cin.peek() != '\n') {
+    return 1;
+  }
+  assert(N > 1);
+  std::cout << "Insert the parameter s for separation \n";
+  double s;
+  std::cin >> s;
+  if (!std::cin.good()) {
+    return 1;
+  }
+  assert(s >= 0);
+  std::cout << "Insert the parameter d \n";
+  double d;
+  std::cin >> d;
+  if (!std::cin.good()) {
+    return 1;
+  }
+  assert(d >= 0);
+  std::cout << "Insert the parameter ds for minimum distance between boids \n";
+  double ds;
+  std::cin >> ds;
+  if (!std::cin.good()) {
+    return 1;
+  }
+  assert(ds >= 0);
+  std::cout << "Insert the parameter a for alignment \n";
+  double a;
+  std::cin >> a;
+  if (!std::cin.good()) {
+    return 1;
+  }
+  assert(a >= 0);
+  std::cout << "Insert the parameter c for cohesion \n";
+  double c;
+  std::cin >> c;
+  if (!std::cin.good()) {
+    return 1;
+  }
+  assert(c >= 0);
+  const double min_position{10.};
+  const double max_position{790.};
+  const double max_velocity{150.};
+  const double velocity_generation{100.};
+  const double frame_rate_limit{1. / 60.};
+  sf::RenderWindow window(sf::VideoMode(800, 800), "FLOCK OF BOIDS");
+  std::vector v =
+      boids_generator(N, min_position, max_position, velocity_generation);
+  auto shapes = create_shapes(v);
+  constexpr int frame_rate{60};
+  window.setFramerateLimit(frame_rate);
+  while (window.isOpen()) {
+    sf::Event event;
+    while (window.pollEvent(event)) {
+      if (event.type == sf::Event::Closed) {
+        window.close();
+      }
+    }
+    v = update_boids(v, s, d, ds, a, c, N, min_position, max_position,
+                     max_velocity, frame_rate_limit);
+    update_window(window, shapes, v);
+    data positon_analysis  = position_data_analysis(v, N);
+    data velocity_analysis = velocity_data_analysis(v, N);
+    std::cout << "\rDistanza media dal centro di massa e velocità media: "
+              << positon_analysis.mean << " +/- " << positon_analysis.sigma
+              << "    " << velocity_analysis.mean << " +/- "
+              << velocity_analysis.sigma;
+  }
+}
